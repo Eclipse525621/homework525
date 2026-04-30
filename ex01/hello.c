6f33248ae1a46de4e456ed63e177002d7324f4a1
Eclipse525621 <pan0621emn@gmail.com>
2026-04-30 23:35:02 +0800

    第二次提交：增加input输入输出显示功能

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 int main() {
-   printf("Hello, World\n");
+   printf("Hello, World!\n");
+   char input[100]; 
+   printf("Please input: ");
+   fgets(input, sizeof(input), stdin);
+   input[strcspn(input, "\n")] = '\0';
+   printf("What you input is: %s\n", input);
+   
    return 0;
 }
